b690cc72d6ac2bf43a569099209c18fffb25b6f1
jekasrs <jekasjeny1012@gmail.com>
2025-09-13 10:19:47 +0300

    basic tests

---
+++ test/test_calculator.py
@@ -0,0 +1,37 @@
+import unittest
+from jenkins_calculator.math import calculate
+
+class TestCalculator(unittest.TestCase):
+    # def setUp(self):
+    #     print("Before each test")
+    
+    # def tearDown(self):
+    #     print("After each test")
+    
+    def test_additions(self):
+        self.assertEqual(calculate(1,2,'+'), 3)
+        self.assertEqual(calculate(-5,2,'+'), -3)
+        self.assertEqual(calculate(0,0,'+'), 0)
+        self.assertEqual(calculate(5,-5,'+'), 0)
+    
+    def test_substractions(self):
+        self.assertEqual(calculate(5,2,'-'), 3)
+        self.assertEqual(calculate(0,0,'-'), 0)
+        self.assertEqual(calculate(-1,1,'-'), -2)
+        self.assertEqual(calculate(1,-1,'-'), 2)
+        
+    def test_multiplies(self):
+        self.assertEqual(calculate(3,7,'*'), 21)
+        self.assertEqual(calculate(0,0,'*'), 0)
+        self.assertEqual(calculate(-1,2,'*'), -2)
+        self.assertEqual(calculate(1,-2,'*'), -2)
+        self.assertEqual(calculate(-10,-10,'*'), 100)
+        
+    def test_divizions(self):
+        self.assertEqual(calculate(10,2,'/'),5)
+        self.assertEqual(calculate(0,2,'/'),0)
+        self.assertEqual(calculate(-100,2,'/'),-50)
+        self.assertEqual(calculate(-100,-2,'/'),50)
+
+        with self.assertRaises(Exception):
+            calculate(5,0,'/')
---
+++ test/test_utils.py
@@ -0,0 +1,18 @@
+import unittest
+from unittest.mock import patch
+from jenkins_calculator.utils import read_number
+
+class TestUserInput(unittest.TestCase):
+    
+    @patch('builtins.input', side_effect=['not a number','10', '-20'])
+    def test_correct_input_number(self, mock_input):
+       number = read_number()
+       self.assertIsNone(number)
+       
+       number = read_number()
+       self.assertIsInstance(number, int)
+       self.assertEqual(number, 10)
+       
+       number = read_number()
+       self.assertIsInstance(number, int)
+       self.assertEqual(number, -20)
